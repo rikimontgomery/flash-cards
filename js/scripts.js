9413b9b6959f99f4f13c33abbd293f1053cc825b
Riki Montgomery <mostriki820@gmail.com>
2017-08-08 20:49:26 -0700

    Add titles to the front of the cards.

--- js/scripts.js
+++ js/scripts.js
@@ -31,4 +31,51 @@ $(document).ready(function() {
     $(".flashcard8").toggleClass("flipped");
   });
 
+  $(".flashcard9").on("click", function() {
+    $(".flashcard9").toggleClass("flipped");
+  });
+
+  $(".flashcard10").on("click", function() {
+    $(".flashcard10").toggleClass("flipped");
+  });
+
+  $(".flashcard11").on("click", function() {
+    $(".flashcard11").toggleClass("flipped");
+  });
+
+  $(".flashcard12").on("click", function() {
+    $(".flashcard12").toggleClass("flipped");
+  });
+
+  $(".flashcard13").on("click", function() {
+    $(".flashcard13").toggleClass("flipped");
+  });
+
+  $(".flashcard14").on("click", function() {
+    $(".flashcard14").toggleClass("flipped");
+  });
+
+  $(".flashcard15").on("click", function() {
+    $(".flashcard15").toggleClass("flipped");
+  });
+
+  $(".flashcard16").on("click", function() {
+    $(".flashcard16").toggleClass("flipped");
+  });
+
+  $(".flashcard17").on("click", function() {
+    $(".flashcard17").toggleClass("flipped");
+  });
+
+  $(".flashcard18").on("click", function() {
+    $(".flashcard18").toggleClass("flipped");
+  });
+
+  $(".flashcard19").on("click", function() {
+    $(".flashcard19").toggleClass("flipped");
+  });
+
+  $(".flashcard20").on("click", function() {
+    $(".flashcard20").toggleClass("flipped");
+  });
 });
