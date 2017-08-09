4196894920a2e1f0c3f9959952494c5adf3d18b0
Riki Montgomery <mostriki820@gmail.com>
2017-08-08 19:41:56 -0700

    Add initial html and css, test a flashcard found online, and create more rows of cards.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".flashcard1").on("click", function() {
+    $(".flashcard1").toggleClass("flipped");
+  });
+
+  $(".flashcard2").on("click", function() {
+    $(".flashcard2").toggleClass("flipped");
+  });
+
+  $(".flashcard3").on("click", function() {
+    $(".flashcard3").toggleClass("flipped");
+  });
+});
